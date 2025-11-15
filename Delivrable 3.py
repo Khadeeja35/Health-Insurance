5d333ee20366e7948ed737a4e8b2652a71624fff
Khadeeja35 <khadeejabibi35@gmail.com>
2025-11-14 20:40:49 -0500

    FINAL UPDATE

--- Delivrable 3.py
+++ Delivrable 3.py
@@ -1,4 +1,4 @@
-#Khadeeja Bibi & Rita Elmahboubi
+# Khadeeja Bibi & Rita Elmahboubi
 
 import pandas as pd
 import seaborn as sns
@@ -9,125 +9,55 @@ df = pd.read_csv("insurance.csv")
 
 # a) Initial data inspection
 
+print(df.head())
 
-df.head()
-#print(df.head())
+print(df.shape)
 
-df.shape
-#print(df.shape)
+print(df.info())
 
-df.info()
-#print(df.info())
+print(df.describe())
 
-df.describe()
-#print(df.describe())
+print(df.sum())
 
-df.sum()
-#print(df.sum())
-
-df.nunique()
-#print(df.nunique())
+print(df.nunique())
 
 # b) Handle duplicate entries
 
-df.duplicated()
-#print(df.duplicated())
+print(df.duplicated())
 
 # c) Identify and manage missing values
 
 df.isnull()
-#print(df.isnull())
+print(df.isnull())
 
 df = df.dropna()
 print(df)
 
 # d) Correct data types and formats
 
-
+#    not needed
 
 #                     3. Univariate non-graphical EDA
 
 # a) Numerical variables (mean, median, mode, standard deviation, variance, skewness, kurtosis and quartiles (0.25, 0.5, 0.75))
 
-#print(df.describe())
+for values in ["age","bmi","children","charges"]:
+    print(df[values].mean())
+    print(df[values].median())
+    print(df[values].mode())
+    print(df[values].std())
+    print(df[values].var())
+    print(df[values].skew())
+    print(df[values].kurt())
+    print(df[values].quantile([0.25, 0.5, 0.75]))
 
 # b) Categorical variables (frequency counts, proportion, mode (most frequent category and the number of unique categories).)
 
-#print(df['sex'].value_counts())
-#print(df['sex'].value_counts(normalize=True))
-#print(df['region'].value_counts())
-#print(df['region'].value_counts(normalize=True))
-#print(df['smoker'].value_counts())
-#print(df['smoker'].value_counts(normalize=True))
-#print(df.mode())
-
-# For age:
-    
-#print(df["age"].mean())
-#print(df["age"].median())
-#print(df["age"].mode())
-#print(df["age"].std())
-#print(df["age"].var())
-#print(df["age"].skew())
-#print(df["age"].kurt())
-#print(df["age"].quantile([0.25, 0.5, 0.75]))
-
-# For bmi:
-
-#print(df["bmi"].mean())
-#print(df["bmi"].median())
-#print(df["bmi"].mode())
-#print(df["bmi"].std())
-#print(df["bmi"].var())
-#print(df["bmi"].skew())
-#print(df["bmi"].kurt())
-#print(df["bmi"].quantile([0.25, 0.5, 0.75]))
-
-# For children
-
-#print(df["children"].mean())
-#print(df["children"].median())
-#print(df["children"].mode())
-#print(df["children"].std())
-#print(df["children"].var())
-#print(df["children"].skew())
-#print(df["children"].kurt())
-#print(df["children"].quantile([0.25, 0.5, 0.75]))
-
-# For charges:
-
-#print(df["charges"].mean())
-#print(df["charges"].median())
-#print(df["charges"].mode())
-#print(df["charges"].std())
-#print(df["charges"].var())
-#print(df["charges"].skew())
-#print(df["charges"].kurt())
-#print(df["charges"].quantile([0.25, 0.5, 0.75]))
-
-# b) Categorical variables (frequency counts, proportion, mode (most frequent category and the number of unique categories).)
-
-# For sex:
-    
-#print(df["sex"].value_counts())
-#print(df["sex"].value_counts(normalize=True))
-#print(df["sex"].mode())
-#print(df["sex"].nunique())
-
-# For smoker:
-
-#print(df["smoker"].value_counts())
-#print(df["smoker"].value_counts(normalize=True))
-#print(df["smoker"].mode())
-#print(df["smoker"].nunique())   
-
-# For region:
-
-#print(df["region"].value_counts())
-#print(df["region"].value_counts(normalize=True))
-#print(df["region"].mode())
-#print(df["region"].nunique())
-    
+for values in ["sex","smoker","region"]:
+    print(df[values].value_counts())
+    print(df[values].value_counts(normalize=True))
+    print(df[values].mode())
+    print(df[values].nunique()) 
 
 #                     4. Univariate Graphical EDA
 
@@ -135,33 +65,33 @@ numerical_cols = ["age","bmi","children", "charges"]
 
 # a) Custom and appropriate number of bins
 
-#for col in numerical_cols:
- #   sns.displot(data = df, x = col, aspect = 1, bins = 5)
+for col in numerical_cols:
+    sns.displot(data = df, x = col, aspect = 1, bins = 5)
 
 # b) Conditioning on other variables
 
-#for col in numerical_cols:
- #   sns.displot (data = df, x = col, hue = "sex", aspect = 1.5)
+for col in numerical_cols:
+    sns.displot (data = df, x = col, hue = "sex", aspect = 1.5)
 
 # c) Stacked histogram
 
-#for col in numerical_cols:
- #   sns.displot(data = df, x = col, hue = "smoker", kind = "hist", multiple = "stack")
+for col in numerical_cols:
+    sns.displot(data = df, x = col, hue = "smoker", kind = "hist", multiple = "stack")
 
 # d) Dodge bars
 
-#for col in numerical_cols:
- #   sns.displot(data = df, x = col, aspect = 1, hue = "region", multiple = "dodge")
+for col in numerical_cols:
+    sns.displot(data = df, x = col, aspect = 1, hue = "region", multiple = "dodge")
 
 # e) Normalized histogram statistics
 
-#for col in numerical_cols:
- #   sns.displot(data = df, x = col, hue = "sex", stat = "density", common_norm = False, aspect = 1)
+for col in numerical_cols:
+   sns.displot(data = df, x = col, hue = "sex", stat = "density", common_norm = False, aspect = 1)
 
 # f) Kernel density estimation
 
-#for col in numerical_cols:
- #   sns.displot(data = df, x = col, bw_adjust = 1.5, kind = "kde")
+for col in numerical_cols:
+    sns.displot(data = df, x = col, bw_adjust = 1.5, kind = "kde")
 
 # g)  Empirical cumulative distributions
 
@@ -173,22 +103,20 @@ for col in numerical_cols:
 
 # a) Make use of this approach at least 3 times with different variables from your dataset
 
-#print(pd.crosstab(df['sex'], df['smoker']))
-#print(pd.crosstab(df['sex'], df['region']))
-#print(pd.crosstab(df['smoker'], df['region']))
+print(pd.crosstab(df['sex'], df['smoker']))
+print(pd.crosstab(df['sex'], df['region']))
+print(pd.crosstab(df['smoker'], df['region']))
 
 
 # b) Now use proportions or percentages rather than raw counts (use the “normalize” parameter from crosstab())
 
-#print(pd.crosstab(df['sex'], df['smoker'],normalize=True ))
-#print(pd.crosstab(df['sex'], df['region'], normalize=True))
-#print(pd.crosstab(df['smoker'], df['region'], normalize=True))
+print(pd.crosstab(df['sex'], df['smoker'],normalize=True ))
+print(pd.crosstab(df['sex'], df['region'], normalize=True))
+print(pd.crosstab(df['smoker'], df['region'], normalize=True))
 
 # c) Generate at least one three-way frequency table (3 or more variables, by giving a list of variables to crosstab() rather than single variables)
 
-#print(pd.crosstab(index=[df['sex'], df['smoker']], columns=df['region'], normalize=True))
-
-# Issue encountered. Had to choose which variables are rows(using index=) and which are colums( using colums=)
+print(pd.crosstab(index=[df['sex'], df['smoker']], columns=df['region'], normalize=True))
 
 #                         6. Multivariate graphical EDA
 
@@ -196,79 +124,79 @@ for col in numerical_cols:
 
 # a) 1 plot using Faceting feature
 
-#sns.relplot(data = df, x = "age", y = "charges", kind = "scatter", col = "smoker", height = 5, aspect = 1.5)
+sns.relplot(data = df, x = "age", y = "charges", kind = "scatter", col = "smoker", hue = "children", height = 5, aspect = 1.5)
 
 # b) 1 plot representing 5 variables at once
 
-#sns.relplot(data = df, x= "age", y = "charges", kind = "scatter", hue = "region", size = "smoker", col = "sex", height = 5, aspect = 1.5)
+sns.relplot(data = df, x= "age", y = "charges", kind = "scatter", hue = "region", size = "smoker", col = "sex", height = 5, aspect = 1.5)
 
 # c) 1 plot using line instead of points
 
-#sns.relplot(data = df, x= "age", y = "charges", kind = "line", hue = "sex", height = 5, aspect = 1.5)
+sns.relplot(data = df, x= "age", y = "charges", kind = "line", hue = "sex", height = 5, aspect = 1.5)
 
 # d) 1 plot illustrating standard deviation
 
-#sns.relplot(data = df, x= "bmi", y = "charges", hue = "smoker", kind = "line", errorbar = "sd", height = 5, aspect = 1.5)
+sns.relplot(data = df, x= "bmi", y = "charges", hue = "smoker", kind = "line", errorbar = "sd", height = 5, aspect = 1.5)
 
 # e) 1 plot including a linear regression
 
-#sns.lmplot (data = df, x= "bmi", y = "charges", hue = "smoker", height = 5, aspect = 1.5)
+sns.lmplot (data = df, x= "bmi", y = "charges", hue = "smoker", height = 5, aspect = 1.5)
 
 # 6.2 Visualizing categorical data
 
 # a) 1 categorical scatter plot with jitter enabled
 
-#sns.catplot (data = df, x= "region", y = "charges", hue = "smoker")
+sns.catplot (data = df, x= "region", y = "charges", hue = "smoker")
 
 # b) 1 categorical scatter plot with jitter disabled
 
-#sns.catplot (data = df, x= "sex",  y = "charges", jitter=False)
+sns.catplot (data = df, x= "sex",  y = "charges", jitter=False)
 
 # c) 1 “beeswarm” plot representing 3 variables
 
-#sns.catplot (data=df, x = "smoker", y = "charges", hue = "sex", kind = "swarm")
+sns.catplot (data=df, x = "smoker", y = "charges", hue = "sex", kind = "swarm")
 
 # d) 1 box plot representing 3 variables
 
-#sns.catplot (data = df, x= "region", y = "charges", hue = "smoker", kind = "box")
+sns.catplot (data = df, x= "region", y = "charges", hue = "smoker", kind = "box")
 
 # e) 1 box plot showing the shape of the distribution (boxenplot())
 
-#sns.catplot (data = df, x = "smoker", y = "charges", kind = "boxen")
+sns.catplot (data = df, x = "smoker", y = "charges", kind = "boxen")
 
 # f) 1 split violin plot representing 3 variables with bandwidth adjusted for better visualization
 
-#sns.catplot(data = df, x = "sex", y = "charges", hue = "smoker", split = True, bw_adjust = 0.5, kind='violin')
+sns.catplot(data = df, x = "sex", y = "charges", hue = "smoker", split = True, bw_adjust = 0.5, kind='violin')
 
 # g) 1 violin plot with scatter points inside the violin shapes
 
-#sns.catplot (data = df, x="smoker", y="charges", inner = "point", kind = "violin")
+sns.catplot (data = df, x="smoker", y="charges", inner = "point", kind = "violin")
 
 # h) 1 bar plot representing 3 variables showing 97% confidence intervals 
 
-#sns.catplot(data=df, x='sex', y='age', hue='smoker', kind='box',errorbar=('ci',97))
+sns.catplot(data=df, x='sex', y='age', hue='smoker', kind='box',errorbar=('ci',97))
 
 # i) 1 point plot representing 3 variables showing 90% confidence intervals and lines in dashed style
 
-#sns.catplot(data=df, x='sex', y='charges', hue='smoker',markers=['^','o'],linestyles=['-','--'], kind='point', errorbar=('ci',90))
+sns.catplot(data=df, x='sex', y='charges', hue='smoker',markers=['^','o'],linestyles=['-','--'], kind='point', errorbar=('ci',90))
 
 # j) 1 bar plot showing the number of observations in each category
 
-#categorical_cols = ["sex", "region", "smoker"]
+categorical_cols = ["sex", "region", "smoker"]
 
-#for col in categorical_cols:
- #   sns.catplot(data=df, x=col, kind='count')
+for col in categorical_cols:
+    sns.catplot(data=df, x=col, kind='count')
 
 # 6.3 Visualizing bivariate distributions
 
 # a) 1 “heatmap” plot representing 2 variables with color intensity bar and adjusted bin width.
 
-#sns.displot(data=df, x='bmi',y='charges',binwidth=(2,10000),cbar=True)
+sns.displot(data=df, x='bmi',y='charges',binwidth=(2,10000),cbar=True)
 
 # b) 1 distribution plot with 2 variables making use of bivariate density contours with amount of curves and its lowest level adjusted (use a kernel density estimation displot()).
 
-#sns.displot(df, x='bmi',y='charges',kind='kde')
+sns.displot(df, x='bmi',y='charges',kind='kde')
 
 # c) 1 “heatmap” plot representing 3 variables, again of kind kde.
 
-#sns.displot(df, x='bmi',y='charges',hue='sex',kind='kde')
+sns.displot(df, x='bmi',y='charges',hue='sex',kind='kde')
